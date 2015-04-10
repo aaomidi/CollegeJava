12f25f86db9233a4ea6efbf4b92a3a9f206c4c80
Amir Omidi <aa_omid@yahoo.com>
2015-04-10 21:54:02 +0430

    Added support for second project.

--- src/main/java/com/aaomidi/Main.java
+++ src/main/java/com/aaomidi/Main.java
@@ -1,6 +1,9 @@
 package com.aaomidi;
 
 import com.aaomidi.projects.project1.Project1;
+import com.aaomidi.projects.project2.Project2;
+
+import java.util.Calendar;
 
 /**
  * Created by Amir on 2015-03-03.
@@ -8,8 +11,11 @@ import com.aaomidi.projects.project1.Project1;
 public class Main {
 
     public static void main(String[] args) {
-        Project1 project1 = new Project1();
-        project1.test(100);
-//        project1.execute();
+        Project1 project1;
+        Project2 project2;
+        project1 = new Project1("Floating Box", 2015, Calendar.MARCH, 3);
+        project2 = new Project2("Projectile", 2015, Calendar.APRIL, 10);
+
+        project2.test(100);
     }
 }
