e47c9837b840510bd96ccae215ae1ce7b4d5f9ea
Amir Omidi <aa_omid@yahoo.com>
2015-04-10 21:53:12 +0430

    Added support for fields for extra information about the projects.

--- src/main/java/com/aaomidi/classes/IProject.java
+++ src/main/java/com/aaomidi/classes/IProject.java
@@ -1,10 +1,26 @@
 package com.aaomidi.classes;
 
+import lombok.Getter;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 /**
  * Created by Amir on 2015-03-03.
  */
 public abstract class IProject {
     public static Trace trace = Trace.trace;
+    @Getter
+    private final String name;
+    @Getter
+    private final Calendar calendar;
+
+    public IProject(String name, int year, int month, int day) {
+        this.name = name;
+        Calendar cal = GregorianCalendar.getInstance();
+        cal.set(year, month, day);
+        this.calendar = cal;
+    }
 
     public abstract void execute();
 
